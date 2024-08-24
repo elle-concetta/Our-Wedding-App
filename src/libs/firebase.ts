2e9c1359518c7e25b0fb94d3c425eaeeeedec0ba
Elizabeth Fassler <128157075+elle-concetta@users.noreply.github.com>
2024-08-24 17:21:53 -0400

    Update firebase.ts

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -7,14 +7,14 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZwXFVwzPt8ZELjykMD2uzO_hOvDqlc1A",
-  authDomain: "wedding-application-c3259.firebaseapp.com",
-  databaseURL: "https://wedding-application-c3259-default-rtdb.firebaseio.com",
-  projectId: "wedding-application-c3259",
-  storageBucket: "wedding-application-c3259.appspot.com",
-  messagingSenderId: "337002873463",
-  appId: "1:337002873463:web:852b95edc9c0dc830c7c72",
-  measurementId: "G-RQZHQCKQVP",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
